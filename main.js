e8b6897d16fd8c2e3aa2ee1c41274f022a56d06f
Reshetniak Maksim <resetnak32@gmail.com>
2023-10-25 15:54:12 +0400

    feat: add audio song birds, img, add funtional when you select a bird, the background and the music of birdsong are switched

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const audio = document.querySelector("audio");
 const playBtn = document.querySelector(".play");
 const button = document.querySelector("button");
-// const animationPause = document.querySelector("");
+const containerHeader = document.querySelector(".container-header");
+const containerMain = document.querySelector(".container-main");
 
 function clickHandler() {
   if (audio.paused) {
@@ -15,5 +16,33 @@ function toggeleBtn() {
   button.classList.toggle("pause");
 }
 
+containerHeader.addEventListener("click", (event) => {
+  const currentBird = event.target.dataset.item;
+  switch (currentBird) {
+    case "solovei":
+      containerMain.classList.toggle("solovei");
+      audio.src = "./audio/solovey.mp3";
+      break;
+    case "drozd":
+      containerMain.classList.toggle("drozd");
+      audio.src = "./audio/drozd.mp3";
+      break;
+    case "zarynka":
+      containerMain.classList.toggle("zarynka");
+      audio.src = "./audio/zarynka.mp3";
+      break;
+    case "javaronok":
+      containerMain.classList.toggle("javaronok");
+      audio.src = "./audio/javoronok.mp3";
+      break;
+    case "slavka":
+      containerMain.classList.toggle("slavka");
+      audio.src = "./audio/slavka.mp3";
+      break;
+
+    default:
+      break;
+  }
+});
 button.addEventListener("click", toggeleBtn);
 playBtn.addEventListener("click", clickHandler);
